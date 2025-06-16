b617294e266a18673d8261467459815b330f960d
Abdullah Shakur <abdullahshakur95@gmail.com>
2025-06-16 19:04:58 +0500

    7:04

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -109,7 +109,6 @@ router.post('/fulfillment', async (req, res) => {
             }
         }
 
-        // Process the webhook
         await processWebhook(req.body);
         
         res.status(200).json({ success: true });
